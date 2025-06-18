bbca3d508027ca8c137d7ebdb6b0766c3ab9bac4
whatss7 <363773602@qq.com>
2025-06-18 13:35:48 +0800

    Texture rework; Allow compost; Recipe change

--- src/main/java/io/github/whatss7/onlyflax/OnlyFlax.java
+++ src/main/java/io/github/whatss7/onlyflax/OnlyFlax.java
@@ -1,6 +1,9 @@
 package io.github.whatss7.onlyflax;
 
+import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("onlyflax")
@@ -8,7 +11,16 @@ public class OnlyFlax {
     public static final String MOD_ID = "onlyflax";
 
     public OnlyFlax() {
-        ModItems.register(FMLJavaModLoadingContext.get().getModEventBus());
-        ModBlocks.register(FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+        eventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            ComposterBlock.COMPOSTABLES.put(ModItems.FLAX_SEEDS.get(), 0.3F);
+            ComposterBlock.COMPOSTABLES.put(ModItems.FLAX.get(), 0.5F);
+        });
     }
 }
