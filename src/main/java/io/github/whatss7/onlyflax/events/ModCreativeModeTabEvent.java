57c5d12d0fd9c513f81e7a3d9cc560c3c4cb9d4d
whatss7 <363773602@qq.com>
2025-06-24 21:56:29 +0800

    Preliminarily ported to 1.20.1

--- src/main/java/io/github/whatss7/onlyflax/events/ModCreativeModeTabEvent.java
+++ src/main/java/io/github/whatss7/onlyflax/events/ModCreativeModeTabEvent.java
@@ -1,25 +1,24 @@
 package io.github.whatss7.onlyflax.events;
 
-
 import io.github.whatss7.onlyflax.OnlyFlax;
 import io.github.whatss7.onlyflax.items.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = OnlyFlax.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeModeTabEvent {
     @SubscribeEvent
-    public static void buildCreativeTabContents(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.NATURAL_BLOCKS) {
+    public static void buildCreativeTabContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
             event.accept(ModItems.FLAX_SEEDS.get());
             event.accept(ModItems.WILD_FLAX.get());
         }
-        if (event.getTab() == CreativeModeTabs.INGREDIENTS) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.FLAX.get());
         }
-        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(ModItems.FLAX_BALE.get());
         }
     }
